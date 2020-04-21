289f8fa85552d718080e8c55d72a99affee387be
LeoRoma <xiajtn@gmail.com>
2020-04-21 17:33:21 +0100

    Ask and Price methods added

--- SimpleShop1/Program.cs
+++ SimpleShop1/Program.cs
@@ -4,9 +4,37 @@ namespace SimpleShop1
 {
     class Program
     {
+        static string Ask(string question)
+        {
+            Console.Write(question + " ");
+            return Console.ReadLine();
+        }
+
+        static double Price(int quantity)
+        {
+            double pricePerUnit;
+            if (quantity >= 100)
+            {
+                pricePerUnit = 1.5;
+            }
+            else if (quantity >= 50)
+            {
+                pricePerUnit = 1.75;
+            }
+            else
+            {
+                pricePerUnit = 2;
+            }
+            return quantity * pricePerUnit;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the cat food store!");
+            string entry = Ask("How many cans are you ordering?");
+            int number = int.Parse(entry);
+            double total = Price(number);
+            Console.WriteLine($"You entered {number} cans, your total is: ${total}");
         }
     }
 }
