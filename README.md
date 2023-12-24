76bdc6594c53a7dadf2b69a7a54408e29b1007c8
AibekKarshiboev <aibek.karshiboev@iaau.edu.kg>
2023-12-25 00:41:09 +0600

    added  a reamde.md file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SimpleShop1
 First project with C#
+
+## Screenshot
+![my code's working manner](image/aibek.png)
--- SimpleShop1/Program.cs
+++ SimpleShop1/Program.cs
@@ -28,13 +28,45 @@ namespace SimpleShop1
             return quantity * pricePerUnit;
         }
 
+        static string GetName()
+        {
+            return Ask("What is your name?");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the cat food store!");
+
+            string name = GetName();
+            
             string entry = Ask("How many cans are you ordering?");
-            int number = int.Parse(entry);
+            int number;
+            
+            // Handling invalid input for the number of cans
+            while (!int.TryParse(entry, out number))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                entry = Ask("How many cans are you ordering?");
+            }
+
             double total = Price(number);
-            Console.WriteLine($"You entered {number} cans, your total is: ${total}");
+            Console.WriteLine($"Hi {name}, you entered {number} cans, your total is: ${total}");
+
+            string feedback = Ask("Did you like our service? (Yes/No)");
+
+            // Handling different feedback responses
+            if (feedback.ToLower() == "yes")
+            {
+                Console.WriteLine($"Thank you for your positive feedback, {name}!");
+            }
+            else if (feedback.ToLower() == "no")
+            {
+                Console.WriteLine($"We're sorry to hear that, {name}. We'll strive to improve!");
+            }
+            else
+            {
+                Console.WriteLine($"Your response '{feedback}' is not recognized. Please provide Yes or No.");
+            }
         }
     }
 }
